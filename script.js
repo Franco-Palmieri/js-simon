7723c787ef4b5d4485dbc5f20e27d83ab3f3741c
Ncesecrede <francescopm95@gmail.com>
2021-06-14 13:43:50 +0100

    Simon Says

---
+++ script.js
@@ -0,0 +1,10 @@
+var randomNum1 = parseInt(Math.floor(Math.random()* 100 + 1));
+var randomNum2 = parseInt(Math.floor(Math.random()* 100 + 1));
+var randomNum3 = parseInt(Math.floor(Math.random()* 100 + 1));
+var randomNum4 = parseInt(Math.floor(Math.random()* 100 + 1));
+var randomNum5 = parseInt(Math.floor(Math.random()* 100 + 1));
+var randomNum = alert(randomNum1 + " " + randomNum2 + " " + randomNum3 + " " + randomNum4 + " " + randomNum5);
+var randomNums = [];
+
+randomNum;
+randomNums.push(randomNum);
